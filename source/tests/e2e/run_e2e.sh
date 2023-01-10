92ba213ff80b6ac5f8a7726eac207b154eaef0d2
Ian Downard <ianwow@amazon.com>
2023-01-10 14:47:30 -0800

    add selenium tests to validate front-end

---
+++ source/tests/e2e/run_e2e.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+###############################################################################
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# PURPOSE:
+#   Run selenium tests.
+#
+# PRELIMINARY:
+#  Deploy the solution before running this script and set the following environment variables:
+#  - AWS_ACCESS_KEY_ID - To authorize AWS CLI commands
+#  - AWS_SECRET_ACCESS_KEY - To authorize AWS CLI commands
+#  - EMAIL - To log into the webui
+#  - PASSWORD - To log into the webui
+#  - DATA_BUCKET_NAME - From which to upload data files for testing
+#
+# USAGE:
+#  ./run_e2e.sh [-h] [-v] --stack-name {STACK_NAME} --region {REGION} [--profile {PROFILE}]
+#    STACK_NAME name of the Cloudformation stack where the solution is running.
+#    REGION needs to be in a format like us-east-1
+#    PROFILE is optional. It's the profile that you have setup in ~/.aws/credentials
+#      that you want to use for AWS CLI commands.
+#
+#    The following options are available:
+#
+#     -h | --help       Print usage
+#     -v | --verbose    Print script debug info
+#
+###############################################################################
+
+trap cleanup_and_die SIGINT SIGTERM ERR
+
+usage() {
+  msg "$msg"
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [--profile PROFILE] --stack-name STACK_NAME --region REGION
+
+Available options:
+
+-h, --help        Print this help and exit (optional)
+-v, --verbose     Print script debug info (optional)
+--stack-name      Name of the Cloudformation stack where the solution is running.
+--region          AWS Region, formatted like us-west-2
+--profile         AWS profile for CLI commands (optional)
+EOF
+  exit 1
+}
+
+cleanup_and_die() {
+  trap - SIGINT SIGTERM ERR
+  echo "Trapped signal."
+  cleanup
+  die 1
+}
+
+cleanup() {
+  # Deactivate and remove the temporary python virtualenv used to run this script
+  if [[ "$VIRTUAL_ENV" != "" ]];
+  then
+    deactivate
+    rm -rf $VENV
+    rm -rf __pycache__
+    echo "------------------------------------------------------------------------------"
+    echo "Cleaning up complete"
+    echo "------------------------------------------------------------------------------"
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+parse_params() {
+  # default values of variables set from params
+  flag=0
+  param=''
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    --stack-name)
+      stack_name="${2}"
+      shift
+      ;;
+    --region)
+      region="${2}"
+      shift
+      ;;
+    --profile)
+      profile="${2}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+  # check required params and arguments
+  [[ -z "${stack_name}" ]] && usage "Missing required parameter: stack-name"
+  [[ -z "${region}" ]] && usage "Missing required parameter: region"
+
+  return 0
+}
+
+parse_params "$@"
+msg "Parameters:"
+msg "- Stack name: ${stack_name}"
+msg "- Region: ${region}"
+[[ ! -z "${profile}" ]] && msg "- Profile: ${profile}"
+
+echo ""
+sleep 3
+
+# Make sure aws cli is installed
+if [[ ! -x "$(command -v aws)" ]]; then
+  echo "ERROR: This script requires the AWS CLI to be installed. Please install it then run again."
+  exit 1
+fi
+
+# Make sure aws cli is authorized
+if [ -z $AWS_ACCESS_KEY_ID ]
+then
+  echo "ERROR: You must set the env variable 'AWS_ACCESS_KEY_ID' with a valid IAM access key id."
+  exit 1
+fi
+
+if [ -z $AWS_SECRET_ACCESS_KEY ]
+then
+  echo "ERROR: You must set the env variable 'AWS_SECRET_ACCESS_KEY' with a valid IAM secret access key."
+  exit 1
+fi
+
+# Make sure aws cli is authorized
+if [ -z $EMAIL ]
+then
+  echo "ERROR: You must set the env variable 'EMAIL' to log into the webui."
+  exit 1
+fi
+
+if [ -z $PASSWORD ]
+then
+  echo "ERROR: You must set the env variable 'PASSWORD' to log into the webui."
+  exit 1
+fi
+
+if [ -z $DATA_BUCKET_NAME ]
+then
+  echo "ERROR: You must set the env variable 'DATA_BUCKET_NAME' from which to upload test data files."
+  exit 1
+fi
+
+# Create and activate a temporary Python environment for this script.
+echo "------------------------------------------------------------------------------"
+echo "Creating a temporary Python virtualenv for this script"
+echo "------------------------------------------------------------------------------"
+python3 -c "import os; print (os.getenv('VIRTUAL_ENV'))" | grep -q None
+if [ $? -ne 0 ]; then
+  echo "ERROR: Do not run this script inside Virtualenv. Type \`deactivate\` and run again.";
+  exit 1;
+fi
+command -v python3
+if [ $? -ne 0 ]; then
+  echo "ERROR: install Python3 before running this script"
+  exit 1
+fi
+echo "Using virtual python environment:"
+VENV=$(mktemp -d) && echo "$VENV"
+command -v python3 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "ERROR: install Python3 before running this script"
+  exit 1
+fi
+python3 -m venv "$VENV"
+source "$VENV"/bin/activate
+pip3 install wheel
+pip3 install --quiet -r requirements.txt
+
+echo "------------------------------------------------------------------------------"
+echo "Running pytest"
+echo "------------------------------------------------------------------------------"
+
+export REGION=$region 
+export STACK_NAME=$stack_name
+export EMAIL=$EMAIL
+export PASSWORD=$PASSWORD 
+export DATA_BUCKET_NAME=$DATA_BUCKET_NAME
+pytest -s -W ignore::DeprecationWarning -p no:cacheproviders
+
+if [ $? -ne 0 ]; then
+  die 1
+fi
+
+cleanup
+echo "Done"
+exit 0
---
+++ source/tests/e2e/test_app.py
@@ -0,0 +1,61 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+@pytest.fixture
+def browser():
+    chrome_options = Options()
+    ####### TESTING - remove headless to see browser actions
+    # Make sure the window is large enough in headless mode so that all
+    # the elements on the page are visible
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--start-maximized")
+    ####### TESTING - remove headless to see browser actions
+    from selenium import webdriver
+
+    browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+    return browser
+
+
+def test_everything(browser, test_environment, stack_resources):
+    browser.implicitly_wait(5)
+    browser.get(stack_resources['UserInterface'])
+    wait = WebDriverWait(browser, 30)
+    ####### Login
+    username_field = browser.find_element("xpath", "/html/body/div/div/div/div/div[2]/div[1]/div/input")
+    username_field.send_keys(test_environment['EMAIL'])
+    password_field = browser.find_element("xpath", "/html/body/div/div/div/div/div[2]/div[2]/input")
+    password_field.send_keys(test_environment['PASSWORD'])
+    browser.find_element("xpath", "/html/body/div/div/div/div/div[3]/span[1]/button").click()
+    # Validate navbar brand
+    xpath = '/html/body/div/div/div/div[1]/nav/a'
+    wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    navbar_brand = browser.find_elements("xpath", xpath)[0].get_attribute("innerText")
+    assert navbar_brand == "Amazon Marketing Cloud uploader from AWS"
+
+    # open Step 1
+    browser.find_element("xpath", "/html/body/div/div/div/div[2]/div/div[1]/div/div/div/a[1]").click()
+    # validate the value shown for s3 bucket
+    element_id = "bucket-input"
+    wait.until(EC.presence_of_element_located((By.ID, element_id)))
+    bucket_value = browser.find_element(By.ID, element_id).get_attribute("placeholder")
+    assert bucket_value == test_environment['DATA_BUCKET_NAME']
+    # validate the s3 bucket table
+    xpath = '/html/body/div/div/div/div[2]/div/div[2]/div[2]/div/table/tbody/tr[1]/td[2]'
+    wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    xpath = '/html/body/div/div/div/div[2]/div/div[2]/div[2]/div/table/tbody/tr'
+    rows = browser.find_elements("xpath", xpath)
+    assert len(rows) > 0
+
+    # Sign out
+    browser.find_element("xpath", "/html/body/div/div/div/div[1]/nav/div/ul/li/a").click()
