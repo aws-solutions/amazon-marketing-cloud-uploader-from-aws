7b8eba231efb67a56e768a86154ba0ae8d8223c3
Immanuel I George <imgeo@amazon.com>
2023-02-14 14:25:41 -0500

    pre-commit

--- source/tests/test_helper.py
+++ source/tests/test_helper.py
@@ -1,8 +1,9 @@
-import pytest
 import json
-import boto3
 import os
-from unittest.mock import MagicMock, patch, mock_open
+from unittest.mock import MagicMock, mock_open, patch
+
+import boto3
+import pytest
 from moto import mock_s3
 
 
@@ -19,7 +20,6 @@ def test_configs():
     }
 
 
-
 @pytest.fixture
 def fake_event(test_configs):
     return {
@@ -31,14 +31,13 @@ def fake_event(test_configs):
             "WebsiteCodeBucket": test_configs["s3_bucket"],
             "WebsiteCodePrefix": test_configs["s3_prefix"],
             "DeploymentBucket": f"{test_configs['s3_bucket']}.some_bucket",
-        }
+        },
     }
+
 
 @pytest.fixture
 def fake_context():
-    return MagicMock(
-        log_stream_name="fake_log_stream"
-    )
+    return MagicMock(log_stream_name="fake_log_stream")
 
 
 @pytest.fixture
@@ -53,20 +52,30 @@ def mock_env_variables(test_configs):
     os.environ["EncryptionMode"] = "Secured"
 
 
-
 @mock_s3
 @patch("urllib.request.build_opener")
 def test_send_response(mock_response, fake_event, fake_context, test_configs):
     from helper.website_helper import send_response
 
-    send_response(event=fake_event, context=fake_context, response_status=test_configs["response_status"], response_data={})
+    send_response(
+        event=fake_event,
+        context=fake_context,
+        response_status=test_configs["response_status"],
+        response_data={},
+    )
 
 
 @mock_s3
 def test_write_to_s3(fake_event, fake_context, test_configs):
     from helper.website_helper import write_to_s3
 
-    write_to_s3(event=fake_event, context=fake_context, bucket=test_configs["s3_bucket"], key=test_configs["s3_key"], body={})
+    write_to_s3(
+        event=fake_event,
+        context=fake_context,
+        bucket=test_configs["s3_bucket"],
+        key=test_configs["s3_key"],
+        body={},
+    )
 
 
 @mock_s3
@@ -78,33 +87,42 @@ def test_copy_source(mock_response, fake_event, fake_context):
 
     copy_source(event=fake_event, context=fake_context)
 
+
 @mock_s3
 @patch("urllib.request.build_opener")
-def test_copy_source_fail_env_key(mock_response, mock_env_variables, fake_event, fake_context, test_configs):
-
+def test_copy_source_fail_env_key(
+    mock_response, mock_env_variables, fake_event, fake_context, test_configs
+):
     from helper.website_helper import copy_source
+
     os.environ.pop("UserPoolId")
 
     copy_source(event=fake_event, context=fake_context)
 
 
 @patch("urllib.request.build_opener")
-def test_copy_source_else(mock_response, mock_env_variables, fake_event, fake_context, test_configs):
+def test_copy_source_else(
+    mock_response, mock_env_variables, fake_event, fake_context, test_configs
+):
     with mock_s3():
         s3 = boto3.client("s3")
         s3.create_bucket(Bucket=test_configs["s3_bucket"])
         s3 = boto3.resource("s3")
         s3_object = s3.Object(
-            test_configs["s3_bucket"], f'{test_configs["s3_prefix"]}/{test_configs["s3_key"]}'
+            test_configs["s3_bucket"],
+            f'{test_configs["s3_prefix"]}/{test_configs["s3_key"]}',
         )
         s3_object.put(Body="{}", ContentType=test_configs["content_type"])
         file_loc = "./webapp-manifest.json"
         manifest_data = [test_configs["s3_key"]]
         from helper.website_helper import copy_source
-        with patch("builtins.open", mock_open(read_data=json.dumps(manifest_data))) as mock_file:
+
+        with patch(
+            "builtins.open", mock_open(read_data=json.dumps(manifest_data))
+        ) as mock_file:
             copy_source(event=fake_event, context=fake_context)
 
-        mock_file.assert_called_with(file_loc, encoding='utf-8')
+        mock_file.assert_called_with(file_loc, encoding="utf-8")
 
 
 @patch("urllib.request.build_opener")
@@ -114,10 +132,12 @@ def test_purge_bucket(mock_response, fake_event, fake_context, test_configs):
         s3.create_bucket(Bucket=test_configs["s3_bucket"])
         s3 = boto3.resource("s3")
         s3_object = s3.Object(
-            test_configs["s3_bucket"], f'{test_configs["s3_prefix"]}/{test_configs["s3_key"]}'
+            test_configs["s3_bucket"],
+            f'{test_configs["s3_prefix"]}/{test_configs["s3_key"]}',
         )
         s3_object.put(Body="{}", ContentType=test_configs["content_type"])
         from helper.website_helper import purge_bucket
+
         purge_bucket(event=fake_event, context=fake_context)
 
 
@@ -128,22 +148,24 @@ def test_lambda_handler(mock_response, fake_event, fake_context, test_configs):
         s3.create_bucket(Bucket=test_configs["s3_bucket"])
         s3 = boto3.resource("s3")
         s3_object = s3.Object(
-            test_configs["s3_bucket"], f'{test_configs["s3_prefix"]}/{test_configs["s3_key"]}'
+            test_configs["s3_bucket"],
+            f'{test_configs["s3_prefix"]}/{test_configs["s3_key"]}',
         )
         s3_object.put(Body="{}", ContentType=test_configs["content_type"])
         file_loc = "./webapp-manifest.json"
         from helper.website_helper import lambda_handler
+
         manifest_data = [test_configs["s3_key"]]
-        from helper.website_helper import copy_source
-        with patch("builtins.open", mock_open(read_data=json.dumps(manifest_data))) as mock_file:
+
+        with patch(
+            "builtins.open", mock_open(read_data=json.dumps(manifest_data))
+        ) as mock_file:
             fake_event["RequestType"] = "Create"
             lambda_handler(event=fake_event, context=fake_context)
 
             fake_event["RequestType"] = "Update"
             lambda_handler(event=fake_event, context=fake_context)
 
-        mock_file.assert_called_with(file_loc, encoding='utf-8')
+        mock_file.assert_called_with(file_loc, encoding="utf-8")
         fake_event["RequestType"] = "Delete"
         lambda_handler(event=fake_event, context=fake_context)
-
-        
