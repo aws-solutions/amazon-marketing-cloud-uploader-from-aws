a09fe2ce2768e1b5199f5260911a20b764091895
Ian Downard <ianwow@amazon.com>
2024-06-20 11:02:16 -0700

    add v3.0.1 release candidate to github

--- source/share/tasks.py
+++ source/share/tasks.py
@@ -268,7 +268,7 @@ class AMCRequests:
             "Authorization": f'Bearer {kwargs["access_token"]}',
             "Content-Type": "application/json",
             "x-amzn-service-name": "amazon-marketing-cloud-uploader-from-aws",
-            "x-amzn-service-version": "v3.0.0"
+            "x-amzn-service-version": "v3.0.1"
         }
 
         if kwargs.get("advertiser_id"):
