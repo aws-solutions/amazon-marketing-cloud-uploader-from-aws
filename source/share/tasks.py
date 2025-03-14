4f7fa922a45fbee15321c3dc56d6994b2f55c733
Yang Qin <yangqqin@amazon.com>
2025-03-14 11:06:57 -0700

    Changes for v3.0.10

--- source/share/tasks.py
+++ source/share/tasks.py
@@ -268,7 +268,7 @@ class AMCRequests:
             "Authorization": f'Bearer {kwargs["access_token"]}',
             "Content-Type": "application/json",
             "x-amzn-service-name": "amazon-marketing-cloud-uploader-from-aws",
-            "x-amzn-service-version": "v3.0.9"
+            "x-amzn-service-version": "v3.0.10"
         }
 
         if kwargs.get("advertiser_id"):
